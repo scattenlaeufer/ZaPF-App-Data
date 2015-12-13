27e7cfcfb2a9c09099ab5460a68d59555b682e0a
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2015-12-13 02:42:49 +0100

    Travis deployment

--- template.py
+++ template.py
@@ -3,7 +3,6 @@
 import sys, json
 import yaml
 
-
 with open(sys.argv[1], 'r') as ak_file:
 	ak_data = yaml.load(ak_file.read())
 
@@ -11,10 +10,6 @@ with open(sys.argv[2], 'r') as slot_file:
 	slot_data = yaml.load(slot_file.read())
 
 out = {'arbeitskreise':ak_data,'slots':slot_data}
-
 json_out = json.dumps(out,sort_keys=True,indent=4)
 
 print(json_out)
-
-with open('output.json','w+') as json_file:
-	json_file.write(json_out)
